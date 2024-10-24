3537b8d6d4061709ef91990a4078f8336029ebbe
Wesley Majully <wesleymajully@hotmail.com>
2024-10-24 17:58:10 -0300

    Imprimindo dados com Jackson e criada classe tipos genérico.

--- src/main/java/br/com/alura/screenmarch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmarch/ScreenmatchApplication.java
@@ -1,6 +1,8 @@
 package br.com.alura.screenmarch;
 
+import br.com.alura.screenmarch.model.DadosSerie;
 import br.com.alura.screenmarch.service.ConsumoApi;
+import br.com.alura.screenmarch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,5 +19,8 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		var consumoApi = new ConsumoApi();
 		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
 		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
 	}
 }
